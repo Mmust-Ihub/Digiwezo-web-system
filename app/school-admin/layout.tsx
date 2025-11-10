1aa1be6ff77ed3e0e104d49c20254d4aa73a3eb0
Mercy Korir <mercykorir@gmail.com>
2025-11-10 09:33:12 +0300

    initialized school admin role

---
+++ app/school-admin/layout.tsx
@@ -0,0 +1,13 @@
+export default function School-adminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="min-h-screen flex flex-col">
+      <header className="p-4 bg-gray-100 border-b">
+        <h1 className="text-xl font-semibold capitalize">school-admin Dashboard</h1>
+      </header>
+
+      <main className="flex-1 p-6">
+        {children}
+      </main>
+    </section>
+  );
+}
