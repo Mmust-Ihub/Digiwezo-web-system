b4174f9f289e5a39b340efafc5cb6e46ea0b52c2
Mercy Korir <mercykorir@gmail.com>
2025-11-11 11:29:33 +0300

    feat(school-admin): add  Sidebar and Navbar

---
+++ app/school-admin/data/sideconstants.ts
@@ -0,0 +1,16 @@
+import {  LayoutDashboard,  FileText,  MessageSquare,  CreditCard,  User,  Users, CalendarCheck,  Settings,  Briefcase
+} from "lucide-react";
+
+export const sideItems = [
+  { title: "Dashboard", icon: LayoutDashboard, path: "/school-admin" },
+  { title: "Classes", icon: Users, path: "/school-admin/classes" },
+  { title: "File Library", icon: FileText, path: "/school-admin/file-library" },
+  { title: "Attendance", icon: CalendarCheck, path: "/school-admin/attendance" },
+  { title: "BOM", icon: CreditCard, path: "/school-admin/bom" },
+  { title: "Teachers", icon: User, path: "/school-admin/teachers" },
+  { title: "Students", icon: Users, path: "/school-admin/students" },
+  { title: "Staffs", icon: Briefcase, path: "/school-admin/staffs" },
+  { title: "Messages", icon: MessageSquare, path: "/school-admin/messages" },
+  { title: "Profile", icon: User, path: "/school-admin/profile" },
+  { title: "Settings", icon: Settings, path: "/school-admin/settings" },
+];
--- app/school-admin/page.tsx
+++ app/school-admin/page.tsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 export default function SchoolAdminPage() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-semibold">School-admin Home</h1>
-      <p>Welcome to the school-admin dashboard.</p>
+      <Header/>
+      <Sidebar/>
     </div>
   );
 }
