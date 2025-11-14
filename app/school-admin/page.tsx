3234b38ab310ff1fa4af558924d1a4afc72e8595
Mercy Korir <mercykorir@gmail.com>
2025-11-14 09:44:18 +0300

    refactor: removed navbar ad sidebar from page.tsx

--- app/school-admin/page.tsx
+++ app/school-admin/page.tsx
@@ -1,5 +1,3 @@
-import { Header } from "@school-admin/components/Header";
-import { Sidebar } from "@school-admin/components/Sidebar";
 import SummaryCards from "./components/SummaryCards";
 import QuickActions from "./components/QuickActions";
 import CalendarSection from "./components/CalendarSection";
@@ -7,9 +5,7 @@ import AdminCard from "./components/AdminCard";
 
 export default function SchoolAdminPage() {
   return (
-    <div className=" flex-1 p-6 ml-64 min-h-screen bg-custom-white">
-      <Header />
-      <Sidebar />
+    <div className=" flex-1 p-6  min-h-screen bg-custom-white">
 
       <div className="flex flex-col lg:flex-row gap-6">
         <div className="w-full lg:w-2/3 flex flex-col gap-6">
