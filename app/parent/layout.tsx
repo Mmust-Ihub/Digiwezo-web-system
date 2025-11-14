f7d798a9829c08ef20d4f8ec49ada4ed6b63d5f6
SebbieMzingKe <sebbievilar2@gmail.com>
2025-11-14 18:03:21 +0300

    fix: routing link for the first element in the parent sidebar.

--- app/parent/layout.tsx
+++ app/parent/layout.tsx
@@ -1,20 +1,23 @@
 import { Header } from "@parent/components/Header";
 import { Sidebar } from "@parent/components/SideBar";
-import { Outlet } from "react-router-dom";
 
-export default function ParentLayout({ children }: { children: React.ReactNode }) {
+export default function ParentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="min-h-screen bg-background">
-      <Sidebar />
-      <Header />
-      
-      <main className="ml-64 mt-16 p-8">
-        {children}
-        <div className="max-w-7xl mx-auto">
-          <Outlet />
+    <div className="w-screen h-screen flex flex-col overflow-auto">
+      <div className="sticky top-0 z-30 h-[12vh]">
+        <Header />
+      </div>
+
+      <div className="flex flex-row">
+        <Sidebar />
+        <div className="h-full w-full bg-background  overflow-x-clip ml-64 p-8 py-6 pb-12">
+          {children}
         </div>
-      </main>
+      </div>
     </div>
   );
-};
-
+}
